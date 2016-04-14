81b451a3e199d94970512402d6ad8996d32ca01a
Siavash Kavousi <siavash.kavousi.sk@gmail.com>
2016-04-14 14:30:00 +0430

    Adds timer0 overflow isr with a winker led (1 sec winker)

--- HW#6/HW6/HW6/Q1-1.asm
+++ HW#6/HW6/HW6/Q1-1.asm
@@ -25,6 +25,13 @@ reset_isr:
 	ldi r17, HIGH(RAMEND)
 	out SPH, r17
 
+	ldi temp, (1 << PC5)
+	out DDRC, temp
+
+	in temp, TIMSK
+	ori temp, (1 << TOIE0)
+	out TIMSK, temp
+
 	in temp, TCCR0
 	ori temp, (1 << CS02) | (0 << CS01) | (1 << CS00)
 	out TCCR0, temp
@@ -38,14 +45,28 @@ timer0_ovf_isr:
 	cli
 
 	inc global_var
-	cpi globar_var, 0x04
-	breq wink_led
+	cpi global_var, 0x04
+	brne timer0_ovf_isr_end
+	
+	call wink_led
+	ldi global_var, 0x00
 
+timer0_ovf_isr_end:
 	sei
 	reti
 
 wink_led:
-	rjmp wink_led
+	sbis PORTC, PC5
+	rjmp wink_led_set_pc5
+	sbic PORTC, PC5
+	rjmp wink_led_clear_pc5
+wink_led_set_pc5:
+	sbi PORTC, PC5
+	rjmp wink_led_end
+wink_led_clear_pc5:
+	cbi PORTC, PC5
+wink_led_end:
+	ret
 
 start:
 	rjmp start
