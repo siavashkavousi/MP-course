956320775f2d473b9bf6be601b5cfed5891e615c
siavash <siavash.kavousi.sk@gmail.com>
2016-04-30 01:24:11 +0430

    Changes AREF to reference voltage (MP)

--- HW#7/HW7/HW7/q2.asm
+++ HW#7/HW7/HW7/q2.asm
@@ -22,7 +22,9 @@ reset_isr:
 	ldi r17, HIGH(RAMEND)
 	out SPH, r17
 
-	sbi ADMUX, REFS0
+	in temp, ADMUX
+	ori temp, (1 <<REFS1) | (1 << REFS0)
+	out ADMUX, temp
 
 	in temp, ADCSRA
 	ori temp, (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0) | (1 << ADSC) | (1 << ADIE) | (1 << ADATE)
@@ -31,17 +33,35 @@ reset_isr:
 	ldi temp, (0 << ADTS2) | (0 << ADTS1) | (0 << ADTS0)
 	out SFIOR, temp
 
+	rcall LCD_init
+
 	sei
 	jmp start
 
 adc_conversion_complete_isr:
 	cli
 
-	
+	in temp, ADCL
+	mov zl, temp
+	in temp, ADCH
+	mov zh, temp
+	// divide z by two
+	rcall divide_by_4
+
+	// print on lcd...
+
+	// mast mali :D
+	sbi ADCSRA, ADSC
 
 	sei
 	reti
 
+divide_by_4:
+	asr zh
+	ror zl
+	asr zh
+	ror zl
+	ret
+
 start:
 	rjmp start
-
